238576fa0779e53448a442ad764b41e16cb956d6
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-03-14 10:15:09 +0100

    Spec revert method added to Version.

--- spec/revertible_paper_trail_spec.rb
+++ spec/revertible_paper_trail_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe RevertiblePaperTrail do
-  it "should be valid" do
+  it "should be a module" do
     RevertiblePaperTrail.should be_a(Module)
+  end
+  
+  it "should add a revert instance method to Version" do
+    Version.new.should respond_to(:revert)
   end
 end
