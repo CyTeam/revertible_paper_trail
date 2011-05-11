cf473b8629555c34a9f66643324b348c1ad337a2
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-05-11 10:22:49 +0200

    Drop most nesting in spec test.

--- spec/revertible_paper_trail_spec.rb
+++ spec/revertible_paper_trail_spec.rb
@@ -9,57 +9,49 @@ describe RevertiblePaperTrail do
     Version.new.should respond_to(:revert)
   end
 
-  context "given a dummy_data object" do
-    let(:data) { build(:dummy_data) }
-    
-    context "when has been saved" do
-      before do
-        data.save
-        @created_version = data.versions.last
-      end
-     
-      it "should revert create" do
-        data_id = data.id
-
-        @created_version.revert
-        
-        DummyData.exists?(data_id).should be_false
-      end
+  context ".revert" do
+    it "should revert create" do
+      data = create(:dummy_data, :trailed_field => 'orig', :untrailed_field => 'orig')
       
-      it "should revert update" do
-        data = create(:dummy_data, :trailed_field => 'orig', :untrailed_field => 'orig')
-        
-        data.trailed_field = 'new'
-        data.save!
-        
-        latest_version = data.versions.last
-        latest_version.revert
-        
-        data.reload
-        data.trailed_field.should == 'orig'
-      end
+      latest_version = data.versions.last
+      latest_version.revert
+      
+      DummyData.exists?(data.id).should be_false
+    end
+    
+    it "should revert update" do
+      data = create(:dummy_data, :trailed_field => 'orig', :untrailed_field => 'orig')
+      
+      data.trailed_field = 'new'
+      data.save!
+      
+      latest_version = data.versions.last
+      latest_version.revert
+      
+      data.reload
+      data.trailed_field.should == 'orig'
+    end
 
-      it "should revert destroy" do
-        data = create(:dummy_data, :trailed_field => 'orig', :untrailed_field => 'orig')
-        
-        data.destroy
-        
-        latest_version = data.versions.last
-        latest_version.revert
-        
-        DummyData.exists?(data.id).should be_true
-      end
+    it "should revert destroy" do
+      data = create(:dummy_data, :trailed_field => 'orig', :untrailed_field => 'orig')
+      
+      data.destroy
+      
+      latest_version = data.versions.last
+      latest_version.revert
+      
+      DummyData.exists?(data.id).should be_true
+    end
 
-      it "should do nothing on revert create of an already destroyed item" do
-        data = create(:dummy_data, :trailed_field => 'orig', :untrailed_field => 'orig')
-        
-        data.destroy
-        
-        first_version = data.versions.first
-        first_version.revert
+    it "should do nothing on revert create of an already destroyed item" do
+      data = create(:dummy_data, :trailed_field => 'orig', :untrailed_field => 'orig')
+      
+      data.destroy
+      
+      first_version = data.versions.first
+      first_version.revert
 
-        DummyData.exists?(data.id).should be_false
-      end
+      DummyData.exists?(data.id).should be_false
     end
   end
 end
